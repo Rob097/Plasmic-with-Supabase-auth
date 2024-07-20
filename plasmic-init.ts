71db657f3b668f65efb1de18b08b6bae745c39b7
Roberto <roberto.dellantonio@fleap.io>
2024-07-20 11:18:14 +0200

    Some tests on supabase auth

--- plasmic-init.ts
+++ plasmic-init.ts
@@ -1,4 +1,6 @@
 import { initPlasmicLoader } from "@plasmicapp/loader-nextjs";
+import { AuthButton } from "@/components/AuthButton";
+import { AuthForm } from "@/components/AuthForm";
 
 export const PLASMIC = initPlasmicLoader({
   projects: [
@@ -12,7 +14,7 @@ export const PLASMIC = initPlasmicLoader({
   // For development, you can set preview to true, which will use the unpublished
   // project, allowing you to see your designs without publishing.  Please
   // only use this for development, as this is significantly slower.
-  preview: false,
+  preview: true,
 });
 
 // You can register any code components that you want to use here; see
@@ -23,3 +25,6 @@ export const PLASMIC = initPlasmicLoader({
 // https://docs.plasmic.app/learn/app-hosting/#set-a-plasmic-project-to-use-your-app-host
 
 // PLASMIC.registerComponent(...);
+
+PLASMIC.substituteComponent(AuthButton, "AuthButton");
+PLASMIC.substituteComponent(AuthForm, "AuthForm");
